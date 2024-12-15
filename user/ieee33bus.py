9647f67082a2f8b462508b5944c46df6be4f7d49
Ahmed Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2024-12-15 23:12:29 +0300

    "regular automatic update"

--- user/ieee33bus.py
+++ user/ieee33bus.py
@@ -1,11 +1,12 @@
 import pandapower as pp
 import pandapower.networks as pn
 
+# case study 33 bus
 net = pn.case33bw()
 pp.runpp(net, trafo_mode="pi")
 
-# Print the bus results
+# # Print the bus results
 print(net.res_bus)
 
-# Print the line results
+# # Print the line results
 print(net.res_line)
